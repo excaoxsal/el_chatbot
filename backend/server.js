6e84d89d67179e46d8b1d59cb74de6d55f378919
excaoxsal <exca561@gmail.com>
2025-05-20 23:47:27 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 const cors = require('cors'); //
 app.use(cors({
-  origin: ['http://localhost', 'http://127.0.0.1'] // Izinkan dari origin ini
+  origin: ['http://localhost', 'http://127.0.0.1']
 }));
 
 app.use(express.json());
